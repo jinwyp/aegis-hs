a4ed53dbf7bb1df9d80bfb56fbec7b209bf07780
hary <94093146@qq.com>
2017-09-14 13:46:19 +0800

    updated

--- src/main/resources/db/migration/V2__yinshou.sql
+++ src/main/resources/db/migration/V2__yinshou.sql
@@ -1,5 +1,5 @@
 -- 应收订单表
-CREATE TABLE hs_yin_order (
+create table hs_yin_order (
    id bigint(20)                    not null auto_increment,
    deptId bigint(20)                not null comment '所属事业部',
    teamId bigint(20)                not null comment '所属团队',
@@ -11,19 +11,19 @@ CREATE TABLE hs_yin_order (
    downstreamId  bigint(20)         not null comment '下游id',
    downstreamSettleMode varchar(20) not null comment '下游结算方式',
    tsc timestamp                    not null default current_timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)engine=InnoDB default charset=utf8;
 
 -- 业务订单-其他参与方
-CREATE TABLE hs_yin_order_party (
+create table hs_yin_order_party (
    id bigint(20)       not null auto_increment,
    orderId bigint(20)  not null comment '订单id, 业务线id',
    custType varchar(32)         comment '客户类型: 上游, 贸易商, 下游, 资金方, 运输公司, 账务公司',
    customerId          not null comment '业务线(订单)关联的其他账务公司',
    tsc timestamp       not null default current_timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)engine=InnoDB default charset=utf8;
 
 -- 业务线(订单)核算月全局配置
-CREATE TABLE hs_yin_order_config (
+create table hs_yin_order_config (
    id bigint(20)                       not null auto_increment,
    orderId bigint(20)                  not null comment '订单id, 业务线id',
    hsMonth char(6)                     not null comment '核算月',
@@ -33,10 +33,10 @@ CREATE TABLE hs_yin_order_config (
    expectHKDays int                    not null comment '预计回款天数',
    wPrice  decimal(10, 2)              not null comment '加权单价',
    tsc timestamp                       not null default current_timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)engine=InnoDB default charset=utf8;
 
 -- 应收订单 - 发运
-CREATE TABLE hs_yin_fayun (
+create table hs_yin_fayun (
   id bigint(20) not null auto_increment,
   orderId bigint(20)              not null comment '订单id, 业务线id',
   hsMonth char(6)                 not null comment '核算月份 yyyymm',
@@ -53,10 +53,10 @@ CREATE TABLE hs_yin_fayun (
   downstreamJHH varchar(64)                comment '上游火运情况下的计划号',
   downstreamShip varchar(64)               comment '上游船运情况下的船号',
   tsc timestamp not null default current_timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)engine=InnoDB default charset=utf8;
 
--- 应收订单 - 付款 :   主账务公司付给上游
-CREATE TABLE hs_yin_fukuan (
+-- 应收订单 - 付款:(主账务公司付给上游)
+create table hs_yin_fukuan (
   id bigint(20)                  not null auto_increment,
   orderId bigint(20)             not null comment '订单id, 业务线id',
   hsMonth char(6)                not null comment '核算月YYYYMM',
@@ -69,10 +69,10 @@ CREATE TABLE hs_yin_fukuan (
   useInterest decimal(10, 4)              comment '当资金方为非自由资金时: 外部资金使用利率',
   useDays int                             comment '当资金方为非自由资金时: 外部资金使用利率',
   tsc timestamp not null default current_timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)engine=InnoDB default charset=utf8;
 
 -- 应收订单 - 回款
-CREATE TABLE hs_yin_huikuan (
+create table hs_yin_huikuan (
    id bigint(20)                    not null auto_increment,
    orderId bigint(20)               not null comment '订单id, 业务线id',
    hkCompanyId bigint(20)           not null comment '回款公司-谁回的款',
@@ -84,77 +84,128 @@ CREATE TABLE hs_yin_huikuan (
    hkDiscountRate decimal(10, 2)             comment '如果回款方式是银行承兑, 贴息率',
    hkPaperExpire datetime                    comment '票据到期日',
    tsc timestamp                    not null default current_timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)engine=InnoDB default charset=utf8;
 
--- 应收订单 - 回款-付款 mapping
-CREATE TABLE hs_yin_huikuan_map (
+-- 应收订单 - 回款-付款mapping
+create table hs_yin_huikuan_map (
    id bigint(20)         not null auto_increment,
    orderId bigint(20)    not null comment '订单id, 业务线id',
    hkId bigint(20)       not null comment '回款id',
    fkId bigint(20)       not null comment '付款id',
    amount decimal(10,2)  not null comment '回款-付款 对应金额',
    tsc timestamp         not null default current_timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)engine=InnoDB default charset=utf8;
 
 -- 应收订单 - 还款
-CREATE TABLE hs_yin_huankuan (
+create table hs_yin_huankuan (
    id bigint(20)           not null auto_increment,
    orderId bigint(20)      not null comment '订单id, 业务线id',
    hkDate datetime         not null comment '还款日期',
    skCompanyId bigint(20)  not null comment '收款单位(资金方), 只有外部资金的情况, 才存在还款',
    hkAmount decimal(10,2)  not null comment '还款总额',
    tsc timestamp           not null default current_timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)engine=InnoDB default charset=utf8;
 
--- 应收订单 - 还款 - 付款 mapping
-CREATE TABLE hs_yin_huankuan_map (
+-- 应收订单 - 还款 - 付款mapping
+create table hs_yin_huankuan_map (
   id bigint(20)            not null auto_increment,
   orderId bigint(20)       not null comment '订单id, 业务线id',
+  hsMonth char(6)          not null comment '核算月',
+
   hkId bigint(20)          not null comment '还款id',
   fkId  bigint(20)         not null comment '还款对应的付款id',
   principal decimal(10, 2) not null comment '本金', 
   amount decimal(10, 2)    not null comment '本金+利息',
   tsc timestamp            not null default current_timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)engine=InnoDB default charset=utf8;
 
 -- 应收订单 - 上游结算
-CREATE TABLE hs_yin_settle_upstream (
+create table hs_yin_settle_upstream (
    id bigint(20)             not null auto_increment,
    orderId bigint(20)        not null comment '订单id, 业务线id',
+  hsMonth char(6)             not null comment '核算月',
+
+   settleDate date           not null comment '结算日期',
    amount decimal(10, 2)     not null comment '结算数量(吨)',
    money decimal(10, 2)      not null comment '结算金额', 
+
    discountType varchar(32)  not null comment '折扣类型: 利率折扣, 金额折扣',
    discountInterest decimal(10, 4)    comment '利率折扣',
    discountDays int                   comment '利率折扣天数',
    discountAmount decimal(10, 2)      comment '金额折扣',
+
    tsc timestamp             not null default current_timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)engine=InnoDB default charset=utf8;
 
 -- 应收订单 - 下游结算
-CREATE TABLE hs_yin_settle_downstream (
+create table hs_yin_settle_downstream (
    id bigint(20)             not null auto_increment,
    orderId bigint(20)        not null comment '订单id, 业务线id',
+  hsMonth char(6)             not null comment '核算月',
+
+   settleDate date           not null comment '结算日期',
+   amount decimal(10, 2)     not null comment '结算数量(吨)',
+   money decimal(10, 2)      not null comment '结算金额', 
+
+   settleGap decimal(10, 2)  not null comment '结算扣吨',
+
    tsc timestamp             not null default current_timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)engine=InnoDB default charset=utf8;
+
+-- 应收订单 - 下游结算-发运map
+create table hs_yin_settle_downstream_map (
+   id bigint(20)             not null auto_increment,
+   settleId bigint(20)       not null comment '下游结算id',
+   fyId bigint(20)           not null comment '对应发运id',
+   deduction decimal(10,2)   not null comment '抵扣发运吨数',
+   tsc timestamp             not null default current_timestamp
+)engine=InnoDB default charset=utf8;
 
 -- 应收订单 - 运输方结算
-CREATE TABLE hs_yin_settle_traffic (
-  id bigint(20)              not null auto_increment,
-  orderId bigint(20)         not null comment '订单id, 业务线id',
-  tsc timestamp              not null default current_timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table hs_yin_settle_traffic (
+  id bigint(20)               not null auto_increment,
+  orderId bigint(20)          not null comment '订单id, 业务线id',
+  hsMonth char(6)             not null comment '核算月',
+
+  settleDate date             not null comment '结算日期',
+  amount decimal(10, 2)       not null comment '结算数量(吨)',
+  money decimal(10, 2)        not null comment '结算金额', 
+  trafficCompanyId bigint(20) not null comment '与哪个运输方结算',
+
+  tsc timestamp               not null default current_timestamp
+)engine=InnoDB default charset=utf8;
 
 -- 应收订单 - 费用
-CREATE TABLE hs_yin_fee (
-  id bigint(20)              not null auto_increment
+create table hs_yin_fee (
+  id bigint(20)              not null auto_increment,
   orderId bigint(20)         not null comment '订单id, 业务线id',
+  hsMonth char(6)            not null comment '核算月yyyymm',
+  name varchar(64)           not null comment '费用科目',
+  amount decimal(10, 2)      not null comment '金额',
   tsc timestamp              not null default current_timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)engine=InnoDB default charset=utf8;
 
 -- 应收订单 - 发票
-CREATE TABLE hs_yin_invoice (
-  id bigint(20)              not null auto_increment
-  orderId bigint(20)         not null comment '订单id, 业务线id',
+create table hs_yin_invoice (
+  id bigint(20)                 not null auto_increment,
+  orderId bigint(20)            not null comment '订单id, 业务线id',
+  hsMonth char(6)               not null comment '核算月yyyymm',
+  invoiceDirection varchar(32)  not null comment '进项 or 销项',
+  invoiceType varchar(32)       not null comment '货款发票 or 运输发票',
+  openDate  datetime            not null comment '开票日期', 
+  openCompanyId  bigint(20)     not null comment '开票单位', 
+  recieverId  bigint(20)        not null comment '收票单位',
+  tsc timestamp                 not null default current_timestamp
+)engine=InnoDB default charset=utf8;
 
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 应收订单 - 发票明细
+create table hs_yin_invoice_detail (
+  id bigint(20)              not null auto_increment,
+  invoiceId bigint(20)       not null comment '发票记录id',
+  invoiceNumber varchar(128) not null comment '发票号',
+  cargoAmount decimal(10,2)  not null comment '发票对应的货物数量(吨)',
+  taxRate decimal(10,2)      not null comment '税率', 
+  tsc timestamp              not null default current_timestamp
+);
+
 
