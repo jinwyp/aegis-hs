555eac8dfa5dd1f6885bb41075be0a2376416059
hary <94093146@qq.com>
2017-09-21 14:34:05 +0800

    merged

--- src/main/java/com/yimei/hs/service/ying/YingFeeService.java
+++ src/main/java/com/yimei/hs/service/ying/YingFeeService.java
@@ -1,6 +1,8 @@
 package com.yimei.hs.service.ying;
 
 import com.yimei.hs.mapper.YingFeeMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,9 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class YingFeeService {
+
+    private static final Logger logger = LoggerFactory.getLogger(YingFeeService.class);
+
 
     @Autowired
     private YingFeeMapper yingFeeMapper;
