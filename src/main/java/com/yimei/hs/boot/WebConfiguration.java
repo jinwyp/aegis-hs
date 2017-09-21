d68fb24d54718a6d7cc4ee9f6efd8849b1111e2a
hary <94093146@qq.com>
2017-09-21 12:19:55 +0800

    updated

--- src/main/java/com/yimei/hs/boot/WebConfiguration.java
+++ src/main/java/com/yimei/hs/boot/WebConfiguration.java
@@ -1,6 +1,7 @@
 package com.yimei.hs.boot;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yimei.hs.ACLInterceptor;
 import com.yimei.hs.boot.support.Java8TimeModule;
 import com.yimei.hs.boot.web.CurrentUserMethodArgumentHandler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,25 +32,26 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Autowired
     protected CurrentUserMethodArgumentHandler currentUserMethodArgumentHandler;
 
+    @Autowired
+    protected ACLInterceptor aclInterceptor;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
         registry.addResourceHandler("/static/**").addResourceLocations("file:./src-web/dist/static/");
-
-
-
         registry.addResourceHandler("/files/**").addResourceLocations("file:../files/");
     }
-
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(currentUserMethodArgumentHandler);
     }
 
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(aclInterceptor).addPathPatterns("/**");
     }
+
 
     @PostConstruct
     private void jacksonConfig() {
