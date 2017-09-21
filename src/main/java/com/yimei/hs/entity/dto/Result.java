da75d1bf1d039c1191a9049858fcaf89a76f4dfc
hary <94093146@qq.com>
2017-09-21 12:01:05 +0800

    updated

--- src/main/java/com/yimei/hs/entity/dto/Result.java
+++ src/main/java/com/yimei/hs/entity/dto/Result.java
@@ -6,8 +6,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Optional;
-
 /**
  * Created by hary on 2017/9/21.
  */
@@ -24,8 +22,12 @@ public class Result<T> {
     private T data;
     private Error error;
 
-    public static final <M> ResponseEntity<Result<M>> error(int code, String message ) {
+    public static final <M> ResponseEntity<Result<M>> error(int code, String message) {
         return new ResponseEntity<Result<M>>(new Result<M>(false, null, new Error(code, message)), HttpStatus.OK);
+    }
+
+    public static final <M> ResponseEntity<Result<M>> error(int code, String message, HttpStatus httpStatus) {
+        return new ResponseEntity<Result<M>>(new Result<M>(false, null, new Error(code, message)), httpStatus);
     }
 
     public static final <T> ResponseEntity<Result<T>> ok(T t) {
@@ -35,11 +37,10 @@ public class Result<T> {
 
 
 /**
- *    Result<Team>(true, Team(1, , 3, 4))
- *    Result<Team>(false, null, Error(50001, "asdfasfasafa"))
- *    ResponseEntity<Result<Team>>()
- *
- *    Result<ArrayList<Team>>(true, Array(Team(1, 2, 3), Team(1,2, 3))
- *    Result<ArrayList<Team>>(true, Array(Team(1, 2, 3), Team(1,2, 3))
- *
+ * Result<Team>(true, Team(1, , 3, 4))
+ * Result<Team>(false, null, Error(50001, "asdfasfasafa"))
+ * ResponseEntity<Result<Team>>()
+ * <p>
+ * Result<ArrayList<Team>>(true, Array(Team(1, 2, 3), Team(1,2, 3))
+ * Result<ArrayList<Team>>(true, Array(Team(1, 2, 3), Team(1,2, 3))
  */
