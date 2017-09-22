73477d26ded0ad94a0f5a8d7d59afecd3d841f68
JinWYP <jinwyp@gmail.com>
2017-09-22 11:26:02 +0800

    update testing

---
+++ testing/README.md
@@ -0,0 +1,12 @@
+## API 自动化测试
+
+1. 进入testing 目录, ``` npm install ``` 安装 [mocha]  等测试工具. [chai]为提供测试断言语法,  [mocha] 为提供运行测试的环境. [superagent] 为http发请求库.
+2. 进入testing 目录, ``` npm start ``` 运行测试.
+
+
+
+[mocha]: https://mochajs.org/
+[chai]: http://chaijs.com/
+[superagent]: https://github.com/visionmedia/superagent
+
+
--- testing/api/adminHome/department.js
+++ testing/api/adminHome/department.js
@@ -55,4 +55,36 @@ describe('事业部门-相关功能', function () {
     })
 
 
+    it('获取某个ID的部门信息', function (done) {
+        server.get('/api/departments/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err)
+                expect(res.body.success).to.equal(true)
+                expect(res.body.data).to.not.equal(null)
+                expect(res.body.data.id).to.be.a('number')
+                expect(res.body.data.name).to.include('管理部')
+                done()
+            })
+    })
+
+
+    it('修改某个ID的部门名称', function (done) {
+        server.put('/api/departments/3')
+            .set('Accept', 'application/json')
+            .send({
+                name: "新的部门" + Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000
+            })
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err)
+                expect(res.body.success).to.equal(true)
+                expect(res.body.data).to.equal(1)
+                done()
+            })
+    })
+
 })
