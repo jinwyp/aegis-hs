a24e06eb197681f8bbd3dab3fa9e038fd942d9cd
hary <94093146@qq.com>
2017-09-22 16:22:38 +0800

    updated

--- src/main/java/com/yimei/hs/entity/dto/ying/PageYingOrderDTO.java
+++ src/main/java/com/yimei/hs/entity/dto/ying/PageYingOrderDTO.java
@@ -1,7 +1,13 @@
 package com.yimei.hs.entity.dto.ying;
 
 import com.yimei.hs.boot.persistence.BaseFilter;
+import com.yimei.hs.enums.CargoType;
+import com.yimei.hs.enums.OrderStatus;
+import com.yimei.hs.enums.SettleMode;
 import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by hary on 2017/9/21.
@@ -9,4 +15,24 @@ import lombok.Data;
 
 @Data
 public class PageYingOrderDTO extends BaseFilter<PageYingOrderDTO> {
+
+    private Long deptId;
+
+    private Long teamId;
+
+    private Long creatorId;
+
+    private Long ownerId;
+
+    private Long mainAccounting;
+
+    private String line;
+
+    private CargoType cargoType;
+
+    private Long upstreamId;
+
+    private Long downstreamId;
+
+    private OrderStatus status;
 }
