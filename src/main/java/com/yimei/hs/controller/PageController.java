4c5c425c8625857ef7deeb56516d36853f556e4d
hary <94093146@qq.com>
2017-09-22 17:13:37 +0800

    updated

--- src/main/java/com/yimei/hs/controller/PageController.java
+++ src/main/java/com/yimei/hs/controller/PageController.java
@@ -1,5 +1,7 @@
 package com.yimei.hs.controller;
 
+import com.yimei.hs.boot.annotation.CurrentUser;
+import com.yimei.hs.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
 
     @GetMapping("/web/index")
-    public String index(Model model) {
+    public String index(@CurrentUser User user) {
         return "/index";
     }
-
 
     /**
      * 管理页面 为了前端路由需要重定向
