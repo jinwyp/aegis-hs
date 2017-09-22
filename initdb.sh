3128920c608ad78864dd50508b3227443dd7d1eb
lubiaook <lubiaook6868@163.com>
2017-09-22 22:59:31 +0800

    update

--- initdb.sh
+++ initdb.sh
@@ -5,4 +5,6 @@ mysql -umysql -pmysql < src/main/resources/db/migration/V1__user.sql;
 mysql -umysql -pmysql < src/main/resources/db/migration/V2__ying.sql;
 mysql -umysql -pmysql < src/main/resources/db/migration/V3__cang.sql;
 mysql -umysql -pmysql < src/main/resources/db/migration/V4__data.sql;
+mysql -umysql -pmysql < src/main/resources/db/migration/V5__test.sql;
+mysql -umysql -pmysql < src/main/resources/db/migration/V6__foreign.sql;
 
