7f9983348439d47d6033c8a686495324efffece5
lubiaook <lubiaook6868@163.com>
2017-09-25 11:43:03 +0800

    update

--- src/main/java/com/yimei/hs/entity/dto/ying/PageYingHuankuanDTO.java
+++ src/main/java/com/yimei/hs/entity/dto/ying/PageYingHuankuanDTO.java
@@ -18,9 +18,38 @@ public class PageYingHuankuanDTO extends BaseFilter<PageYingHuankuanDTO> {
     private LocalDateTime huankuanDate;
     private BigDecimal huankuanAmount;
 
+    /**
+     * <if test="orderId!=null">t1.orderId=#{orderId}</if>
+     <if test="hsId!=null">t1.hsId=#{hsId}</if>
+     <if test="skCompanyId!=null">t1.skCompanyId=#{skCompanyId}</if>
+     <if test="huankuanDate!=null">t1.huankuanDate=#{huankuanDate}</if>
+     <if test="huankuanAmount!=null">t1.huankuanAmount=#{huankuanAmount}</if>
+     * @param sql
+     * @return
+     */
     @Override
     public String getCountSql(String sql) {
-        return getCountSqlForResultMap(sql);
+
+        System.out.println("orignal sql = " + sql);
+        StringBuilder sb = new StringBuilder("select * from hs_ying_huankuan ");
+        if (orderId != null) {
+            sb.append(" where orderId = ?");
+        }
+        if (hsId != null) {
+            sb.append( " and hsId = ?");
+        }
+        if (skCompanyId != null) {
+            sb.append(" and skCompanyId = ?");
+        }
+        if (huankuanDate != null) {
+            sb.append(" and huankuanDate = ?");
+        }
+        if( huankuanAmount != null) {
+            sb.append(" and huankuanAmount = ?");
+        }
+        String countSql = super.getCountSql(sb.toString());
+        System.out.println("count sql = " + countSql);
+        return countSql;
     }
 
 }
--- src/main/java/com/yimei/hs/entity/dto/ying/PageYingSettleDownstreamDTO.java
+++ src/main/java/com/yimei/hs/entity/dto/ying/PageYingSettleDownstreamDTO.java
@@ -16,9 +16,28 @@ public class PageYingSettleDownstreamDTO extends BaseFilter<PageYingSettleDownst
     private Long hsId;
     private LocalDateTime settleDate;
 
+    /**
+     * @param sql
+     * @return
+     */
     @Override
     public String getCountSql(String sql) {
-        return getCountSqlForResultMap(sql);
+
+        System.out.println("orignal sql = " + sql);
+        StringBuilder sb = new StringBuilder("select * from hs_ying_settle_downstream ");
+        if (orderId != null) {
+            sb.append(" where orderId = ?");
+        }
+        if (hsId != null) {
+            sb.append( " and hsId = ?");
+        }
+        if (settleDate != null) {
+            sb.append(" and settleDate = ?");
+        }
+
+        String countSql = super.getCountSql(sb.toString());
+        System.out.println("count sql = " + countSql);
+        return countSql;
     }
 
 }
