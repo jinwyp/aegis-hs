1086526829afaa5aad4936f46b67826b6535e2fc
hary <94093146@qq.com>
2017-09-25 15:48:59 +0800

    updated

--- src/main/java/com/yimei/hs/user/controller/AdminController.java
+++ src/main/java/com/yimei/hs/user/controller/AdminController.java
@@ -1,8 +1,15 @@
 package com.yimei.hs.user.controller;
 
+import com.yimei.hs.boot.PageResult;
+import com.yimei.hs.boot.Result;
 import com.yimei.hs.boot.annotation.IsAdmin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.yimei.hs.user.dto.PageUserDTO;
+import com.yimei.hs.user.entity.User;
+import com.yimei.hs.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by hary on 2017/9/25.
@@ -12,4 +19,48 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/admin")
 public class AdminController {
 
+
+    @Autowired
+    UserService userService;
+
+    /**
+     *
+     * @param user
+     * @return
+     */
+    @PostMapping("/users")
+    public ResponseEntity<Result<User>> create(@RequestBody @Validated User user) {
+        User nuser = userService.register(user);
+        if (nuser == null) {
+           return Result.error(4001, "创建用户失败");
+        } else {
+            return Result.ok(nuser);
+        }
+    }
+
+
+
+    /**
+     * 查询用户, 分页
+     *
+     * @return
+     */
+    @GetMapping("/users")
+    public ResponseEntity<PageResult<User>> list(PageUserDTO pageUserDTO) {
+        return PageResult.ok(userService.getPage(pageUserDTO));
+    }
+
+
+    /**
+     * 更新用户信息 - 非密码
+     *
+     * @return
+     */
+    @PutMapping("/users")
+    public ResponseEntity<Result<Integer>> update(@RequestBody User user) {
+        userService.update(user);
+        return Result.ok(1);
+    }
 }
+
+
