a28e20713297f06890c816dd9956fa2f7129f3f3
hary <94093146@qq.com>
2017-10-10 14:45:57 +0800

    updated

--- src/main/java/com/yimei/hs/ying/entity/YingOrder.java
+++ src/main/java/com/yimei/hs/ying/entity/YingOrder.java
@@ -62,7 +62,6 @@ public class YingOrder implements Serializable {
     @Null(groups = {CreateGroup.class, UpdateGroup.class}, message = "创建时间由数据库决定")
     private LocalDateTime tsc;
 
-    @Valid()
     private List<YingOrderConfig> orderConfigList;   // 关联的核算月配置
 
     @Valid()
