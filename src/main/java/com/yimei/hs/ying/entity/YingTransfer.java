5ee9b84c34730bc0f22506d20fcc3309d617ca7c
hary <94093146@qq.com>
2017-10-10 14:25:41 +0800

    updated

--- src/main/java/com/yimei/hs/ying/entity/YingTransfer.java
+++ src/main/java/com/yimei/hs/ying/entity/YingTransfer.java
@@ -4,30 +4,27 @@ import com.yimei.hs.boot.api.CreateGroup;
 import com.yimei.hs.boot.api.UpdateGroup;
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class YingTransfer implements Serializable {
 
-    @NonNull
     private Long id;
 
-    @NonNull
+    @NotNull(groups = {CreateGroup.class}, message = "业务线不能为空")
     private Long orderId;
 
-    @NonNull
+    @NotNull(groups = {CreateGroup.class},  message = "转出方不能为空")
     private Long fromUserId;
 
-    @NonNull
+    @NotNull(groups = {CreateGroup.class}, message = "转入方不能为空")
     private Long toUserId;
 
     @Null(groups = {CreateGroup.class, UpdateGroup.class}, message = "创建时间由数据库决定")
     private LocalDateTime tsc;
 
     private static final long serialVersionUID = 1L;
-
 }
