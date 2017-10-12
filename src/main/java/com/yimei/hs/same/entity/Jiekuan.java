411543c422ee234b9f9e0bfc8aa498bf64cb613a
lubiaook <lubiaook6868@163.com>
2017-10-12 13:29:26 +0800

    update

--- src/main/java/com/yimei/hs/same/entity/Jiekuan.java
+++ src/main/java/com/yimei/hs/same/entity/Jiekuan.java
@@ -1,30 +1,44 @@
 package com.yimei.hs.same.entity;
 
+import com.yimei.hs.boot.api.CreateGroup;
+import com.yimei.hs.boot.api.UpdateGroup;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
 public class Jiekuan implements Serializable {
+
+    @NotNull(groups = {UpdateGroup.class}, message = "借款id不能为空")
     private Long id;
 
     private Long fukuanId;
-
+    @NotNull(groups = {CreateGroup.class}, message = "订单id不能为空")
     private Long orderId;
 
+    @NotNull(groups = {CreateGroup.class}, message = "核算月id不能为空")
     private Long hsId;
 
+    @NotNull(groups = {CreateGroup.class}, message = "借款金额不能为空")
     private BigDecimal amount;
 
+    @NotNull(groups = {CreateGroup.class}, message = "资金方id不能为空")
     private Long capitalId;
 
+    @NotNull(groups = {CreateGroup.class}, message = "资金使用利率不能为空")
     private BigDecimal useInterest;
 
+    @NotNull(groups = {CreateGroup.class}, message = "资金使用天数不能为空")
     private Integer useDays;
 
     private Boolean deleted;
+
+    @NotNull(groups = {CreateGroup.class}, message = "借款日期不能为空")
+    private LocalDateTime jiekuanDate;
 
     private Date tsc;
 
