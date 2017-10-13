e5444a66c70c025f121bb3d19ce916a3724ab066
hary <94093146@qq.com>
2017-10-13 11:59:33 +0800

    updated

--- initdb.sh
+++ initdb.sh
@@ -2,9 +2,8 @@
 
 echo "drop database hsdb; create database hsdb;" | mysql -umysql -pmysql;
 mysql -umysql -pmysql < src/main/resources/db/migration/V1__user.sql;
-mysql -umysql -pmysql < src/main/resources/db/migration/V2__ying.sql;
-mysql -umysql -pmysql < src/main/resources/db/migration/V3__cang.sql;
-mysql -umysql -pmysql < src/main/resources/db/migration/V4__data.sql;
-# mysql -umysql -pmysql < src/main/resources/db/migration/V5__test.sql;
-# mysql -umysql -pmysql < src/main/resources/db/migration/V6__foreign.sql;
+mysql -umysql -pmysql < src/main/resources/db/migration/V2__same.sql;
+mysql -umysql -pmysql < src/main/resources/db/migration/V3__ying.sql;
+mysql -umysql -pmysql < src/main/resources/db/migration/V4__cang.sql;
+mysql -umysql -pmysql < src/main/resources/db/migration/V5__data.sql;
 
