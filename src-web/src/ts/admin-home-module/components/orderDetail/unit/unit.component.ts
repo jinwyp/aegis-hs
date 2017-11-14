ca680e58ed68262dad6e46ea889e8f19d8bba614
JinWYP <jinwyp@gmail.com>
2017-11-14 10:24:48 +0800

    update

--- src-web/src/ts/admin-home-module/components/orderDetail/unit/unit.component.ts
+++ src-web/src/ts/admin-home-module/components/orderDetail/unit/unit.component.ts
@@ -202,7 +202,7 @@ export class UnitComponent implements OnInit {
 
                 'expectHKDays'    : '',
                 'tradeAddPrice'    : '',
-                'weightedPrice'    : ''
+                'weightedPrice'    : 99
             })
 
         } else {
